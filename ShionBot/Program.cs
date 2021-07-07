78e4f8a4fa188e9e4193a0da5a52f8d95d78bfce
Jacky Tsoi <jackytsoi2015@outlook.com>
2021-07-06 20:48:40 -0700

    Created base solution files

---
+++ ShionBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShionBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
