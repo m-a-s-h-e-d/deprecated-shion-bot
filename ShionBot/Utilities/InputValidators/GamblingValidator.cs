0a8c864680db6915ed9509a3ab82adb06a35e7cd
Jacky Tsoi <jackytsoi2015@outlook.com>
2021-07-15 16:58:35 -0700

    Create validator class for bet amounts

---
+++ ShionBot/Utilities/InputValidators/GamblingValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShionBot.Utilities.Validators
+{
+    class GamblingValidator
+    {
+        //TODO Input validator for bet amount inputs (Percentages, and perhaps math functions?)
+    }
+}
