e6157a71646bdc672c96d0be17d299f13a915985
Jacky Tsoi <jackytsoi2015@outlook.com>
2021-07-19 15:13:14 -0700

    Add constants for GamblingValidator

--- ShionBot/Utilities/InputValidators/GamblingValidator.cs
+++ ShionBot/Utilities/InputValidators/GamblingValidator.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace ShionBot.Utilities.Validators
 {
-    class GamblingValidator
+    public class GamblingValidator
     {
         //TODO Input validator for bet amount inputs (Percentages, and perhaps math functions?)
+        public static readonly double HalfPercent = 0.5;
+        public static readonly double QuarterPercent = 0.25;
+
     }
 }
