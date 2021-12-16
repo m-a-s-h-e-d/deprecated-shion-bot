94c1f4fd7a8798a37f0c1b827aa44b5a2d7339b9
Jacky Tsoi <jackytsoi2015@outlook.com>
2021-12-16 00:50:26 -0800

    Create base DbContext and factory for ShionDbContext

--- Shion.Data/Context/ShionDbContext.cs
+++ Shion.Data/Context/ShionDbContext.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Shion.Data.Context
 {
-    internal class ShionDbContext
+    /// <summary>
+    /// The class representing the <see cref="DbContext"/> for the Shion project.
+    /// </summary>
+    public class ShionDbContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShionDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The <see cref="DbContextOptions"/> to be passed.</param>
+        public ShionDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
     }
 }
---
+++ Shion.Data/Context/ShionDbContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Shion.Data.Context
+{
+    /// <summary>
+    /// The <see cref="IDbContextFactory"/> that produces a preconfigured <see cref="ShionDbContext"/> for the Shion project.
+    /// </summary>
+    public class ShionDbContextFactory : IDesignTimeDbContextFactory<ShionDbContext>
+    {
+        /// <summary>
+        /// Creates a <see cref="ShionDbContext"/> with the configurations passed in the arguments.
+        /// </summary>
+        /// <param name="args">The <see cref="string"/> arguments to be passed.</param>
+        /// <returns>A <see cref="ShionDbContext"/> representing the configured custom <see cref="DbContext"/>.</returns>
+        public ShionDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder()
+                .UseMySql(
+                configuration.GetConnectionString("Default"),
+                new MySqlServerVersion(new Version(8, 0, 24)));
+
+            return new ShionDbContext(optionsBuilder.Options);
+        }
+    }
+}
